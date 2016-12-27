f66570a39546f5b97ed9249c2c3a1a69ebb034d7
Yanmin XING <ymxing@thoughtworks.com>
2016-12-27 23:51:33 +0800

    add demultiplexer roles to manage resources synchronous

---
+++ src/main/java/Demultiplexer.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.*;
+
+public class Demultiplexer {
+    private ArrayBlockingQueue<Integer> resources = new ArrayBlockingQueue<Integer>(3);
+
+    {
+        resources.add(1);
+        resources.add(2);
+        resources.add(3);
+    }
+
+    public synchronized Integer getResource(long currentTimeMillis, int timeout) {
+        while (true) {
+            if (System.currentTimeMillis() - currentTimeMillis > timeout * 1000) {
+                throw new RuntimeException("Get resource timeout!");
+            } else if (resources.size() > 0) {
+                return resources.poll();
+            }
+        }
+    }
+
+    public synchronized void freeResource(Integer resourceId) {
+        resources.add(resourceId);
+    }
+
+    public void accept(int requestID) {
+        long currentTimeMillis = System.currentTimeMillis();
+        Integer resourceID = getResource(currentTimeMillis, 5);
+    }
+
+}
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
+    private Demultiplexer demultiplexer = new Demultiplexer();
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(4000);
         Runtime.getRuntime().addShutdownHook(new ShutDownHook(serverSocket));
@@ -9,8 +11,9 @@ public class Server {
         while (System.currentTimeMillis() - listenTime < 60000) {
             Socket socket = serverSocket.accept();
             int requestID = socket.getInputStream().read();
-            if (requestID != -1){
+            if (requestID != -1) {
                 System.out.println("No. " + requestID + " request has came!");
+                new Server().demultiplexer.accept(requestID);
             }
         }
         serverSocket.close();
